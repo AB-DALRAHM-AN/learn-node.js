a6fd14efabee9bf188b6ea1a9ee08e24637f2de0
A.A.A <abdalrahman.webdev@gmail.com>
2024-08-29 15:48:29 +0300

    json data, nodemon, ts-node

--- index.js
+++ index.js
@@ -3,9 +3,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 // const http = require('http'); // => CommonJS Module
 var http = require("http"); // => ES6 Module
+var data = {
+    name: "John Doe",
+    age: 25,
+    email: "aaa@gmail.com",
+    address: {
+        city: "New York",
+        state: "NY",
+        country: "USA",
+    },
+};
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<div style='background-color: red'><span>Hello, World</span></div>"); // res.write => Write the Response Body
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.write(data); // res.write => Write the Response Body
     res.end("<div>End</div>"); // res.end => End the Response and Send the Response Body
 });
 var port = 5000; // Port Number
--- index.ts
+++ index.ts
@@ -2,12 +2,24 @@
 
 // const http = require('http'); // => CommonJS Module
 import * as http from "http"; // => ES6 Module
+import { json } from "stream/consumers";
+
+const data = JSON.stringify({
+  name: "John Doe",
+  age: 25,
+  email: "aaa@gmail.com",
+  isStudent: true,
+  subjects: ["Math", "Science", "English"],
+  address: {
+    city: "New York",
+    state: "NY",
+    country: "USA",
+  },
+});
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write(
-    "<div style='background-color: red'><span>Hello, World</span></div>"
-  ); // res.write => Write the Response Body
+  res.writeHead(200, { "Content-Type": "text/json" });
+  res.write(data); // res.write => Write the Response Body
   res.end("<div>End</div>"); // res.end => End the Response and Send the Response Body
 });
 
