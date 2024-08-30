32b29754865cb57b3bbadc7f90772c7d6ad73ca2
A.A.A <abdalrahman.webdev@gmail.com>
2024-08-31 02:24:55 +0300

    route param with products

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,30 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const products = [
+  { id: 1, name: "product 1" },
+  { id: 2, name: "product 2" },
+  { id: 3, name: "product 3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
+app.get("/api/products", (req, res) => {
+  res.send(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p.id === +req.params.id);
+  if (!product) {
+    res.status(404).send("Product not found");
+  }
+  res.send(product);
+});
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });
