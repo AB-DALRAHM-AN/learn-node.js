9d8573b161b4c47f77c17b5424c705a5f57f6b2a
A.A.A <abdalrahman.webdev@gmail.com>
2024-08-31 01:54:28 +0300

    run server

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World');
 });
 
-app.listen(() => {
+const PORT = 5000;
+
+app.listen(PORT, () => {
   console.log('Server is running on port 5000');
 });
